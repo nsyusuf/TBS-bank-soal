f247f47b5afb3a6b371091203b34885c5b3a4b5a
nsy <69889767+nsyusuf@users.noreply.github.com>
2026-08-29 23:09:38 +0800

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TBSBankSoal from './TBSBankSoal';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TBSBankSoal />
+  </React.StrictMode>,
+);
